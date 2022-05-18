64402f2f3bfe1ba122408059741cc3357242e736
Eddy <60142434+EdwinAtieno@users.noreply.github.com>
2022-05-18 11:05:55 +0300

    New practicals

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-"""Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+def addition(f_number, s_number):
+    count= f_number+s_number
+    print(count)
 
-The binary number returned should be a string."""
-#code
-def add_binary(a,b):
-    #your code here
-    c=bin(a+b).replace("0b", "")
-    bin_c = str(c)
-    return bin_c
+def subtraction(f_number,s_number):
+    count = f_number - s_number
+    print(count)
 
 
+def multiplication(f_number,s_number):
+    count = f_number * s_number
+    print(count)
 
+def modulus(f_number, s_number):
+    count = f_number % s_number
+    print(count)
 
+def division(f_number, s_number):
+    count = f_number / s_number
+    print(count)
 
+def initially():
+    f_number = int(input("Please state the first number to input:\t"))
+    s_number = int(input("Please state the first number to input:\t"))
+    process = str(input("Please process to perform, multiplication, addition, subtraction, modulus, division:\t")).lower()
+    if process == "multiplication":
+        addition(f_number, s_number)
+    elif process == "subtraction":
+        subtraction(f_number, s_number)
+    elif process == "division":
+        division(f_number, s_number)
+    elif process == "addition":
+        modulus(f_number, s_number)
+    else:
+        multiplication(f_number, s_number)
+    # multiplication(Nlist)
 
-
-
-
-
+if __name__ == '__main__':
+    initially()
